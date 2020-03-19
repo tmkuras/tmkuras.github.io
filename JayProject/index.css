cd386bc70027791d0de57c11914c5705b4fc90b6
tmkuras <62308628+tmkuras@users.noreply.github.com>
2020-03-19 16:52:33 -0400

    Files

---
+++ JayProject/index.css
@@ -0,0 +1,69 @@
+* {
+  font-family: 'Roboto', sans-serif;
+}
+p {
+  text-align: center;
+  margin: 10px 10px;
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+}
+#download {
+  margin: 10px 0px;
+  border: 2px solid black;
+  border-radius: 5px;
+  background-color: green;
+  width: 30%;
+  height: 4em;
+  color: white;
+  font-size: medium;
+}
+.label {
+  text-decoration: underline;
+  margin: 5px 5px;
+}
+.circle {
+  position: absolute;
+  top: -10px;
+  right: -7px;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  border: 2px solid black;
+  background-color: white;
+}
+.circle > p {
+  margin: 1px 0;
+  font-size: small;
+}
+.state {
+  width: 30%;
+}
+.event {
+  flex: 1 1 0;
+}
+.box {
+  position: relative;
+  margin: 5px 2px;
+  border: 2px solid black;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.clock {
+  font-size: medium;
+}
+.active {
+  background-color: rgba(255, 0, 0, 0.6);
+}
+.flex-row {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.flex-column {
+  display: flex;
+  flex-direction: column;
+}
